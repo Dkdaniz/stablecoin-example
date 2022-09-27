57bf6456947a2cdec1270ae694cb3c314d94b20d
Daniel Marques <daniel@descentralizado.com.br>
2022-09-27 15:58:19 -0300

    feat: add blacklist

--- contracts/Stablecoin.sol
+++ contracts/Stablecoin.sol
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./AccessControl.sol";
+import "./ERC20.sol";
+import "./ERC20Burnable.sol";
+import "./Pausable.sol";
 
-contract Stablecoin is ERC20, ERC20Burnable, Pausable, AccessControl {
+contract Stablecoin is ERC20, Pausable, AccessControl {
   bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+  mapping (address => bool) public isBlackListed;
+
+  event DestroyedBlackFunds(address _blackListedUser, uint _balance);
+  event AddedBlackList(address _user);
+  event RemovedBlackList(address _user);
 
   constructor() ERC20("Stablecoin", "COIN") {
     _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
@@ -20,6 +26,10 @@ contract Stablecoin is ERC20, ERC20Burnable, Pausable, AccessControl {
     return 4;
   }
 
+  function getBlackListStatus(address _maker) external view returns (bool) {
+    return isBlackListed[_maker];
+  } 
+
   function pause() public onlyRole(PAUSER_ROLE) {
     _pause();
   }
@@ -30,6 +40,28 @@ contract Stablecoin is ERC20, ERC20Burnable, Pausable, AccessControl {
 
   function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
     _mint(to, amount);
+  }
+
+  function redeem(uint amount) public onlyRole(DEFAULT_ADMIN_ROLE) {
+    _redeem(amount);
+  }
+  
+  function addBlackList (address _evilUser) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    isBlackListed[_evilUser] = true;
+    emit AddedBlackList(_evilUser);
+  }
+
+  function removeBlackList (address _clearedUser) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    isBlackListed[_clearedUser] = false;
+    emit RemovedBlackList(_clearedUser);
+  }
+
+  function destroyBlackFunds (address _blackListedUser) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    require(isBlackListed[_blackListedUser], "Stablecoin: user dont is in blacklist");
+    
+    uint dirtyFunds = balanceOf(_blackListedUser);
+    _destroyFundsAccount(_blackListedUser, dirtyFunds);
+    emit DestroyedBlackFunds(_blackListedUser, dirtyFunds);
   }
 
   function _beforeTokenTransfer(
--- test/Stablecoin.js
+++ test/Stablecoin.js
@@ -4,15 +4,15 @@ const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { ethers } = require('hardhat');
 const { expect } = chai;
 
-const getAccounts = async () => {
-  const accounts = await ethers.getSigners();
-  return accounts;
-};
+const getSigners = async () => {
+  const [owner, addr1, addr2] = await ethers.getSigners();
+  return { owner, addr1, addr2 }; 
+}
 
 async function deployStablecoin() {
-  const Stablecoin = await ethers.getContractFactory("Stablecoin");
-  const [owner, addr1, addr2] = await ethers.getSigners();
+  const { owner, addr1, addr2 } = await  getSigners()
 
+  const Stablecoin = await ethers.getContractFactory("Stablecoin");
   const stablecoin = await Stablecoin.deploy();
 
   await stablecoin.deployed();
@@ -22,7 +22,7 @@ async function deployStablecoin() {
 
 describe('Stablecoin', function() {
   describe('Decimals', function() {
-    it("Should returns decimals equal 4", async function () {
+    it("Should returns decimals equal 4", async () => {
       const { stablecoin } = await loadFixture(deployStablecoin);
       const decimals = await stablecoin.decimals();
       expect(decimals).to.equal(4);
@@ -84,5 +84,147 @@ describe('Stablecoin', function() {
     });
   })
 
+  describe('Redeem', async () => {
+    it("Should returns error role access",  async () => {
+      const { stablecoin, addr1, } = await loadFixture(deployStablecoin);
+
+      await expect(stablecoin.connect(addr1).redeem(1)).to.be.revertedWith(/AccessControl: account .* is missing role .*/)
+    });
+
+    it("Should returns error when amount exceeds total supply",  async () => {
+      const { stablecoin, owner, } = await loadFixture(deployStablecoin);
+      await expect(stablecoin.connect(owner).redeem(ethers.utils.parseUnits('10', 'ether'))).to.be.revertedWith("ERC20: redeem amount exceeds total supply")
+    });
+
+    context('Trying redeem to token', async () => {
+      before(async () => {
+        const { stablecoin, owner, addr1 } = await loadFixture(deployStablecoin);
+
+        await stablecoin.connect(owner).mint(addr1.address, ethers.utils.parseUnits('1', 'ether'));
+
+        this.stablecoin = stablecoin;
+      });
+    
+      it("Should returns error when amount exceeds balance",  async () => {
+        const { owner } = await getSigners()
+        await expect(this.stablecoin.connect(owner).redeem(ethers.utils.parseUnits('1', 'ether'))).to.be.revertedWith("ERC20: redeem amount exceeds balance")
+      });
+
+      it("Should returns balance of 1 ether to owner after the transfer function is invoked by the addr1 wallet",  async () => {
+        const { owner, addr1 } = await getSigners()
+
+        await this.stablecoin.connect(addr1).transfer(owner.address, ethers.utils.parseUnits('1', 'ether'));
+        expect(await this.stablecoin.connect(owner).balanceOf(owner.address)).to.equal(ethers.utils.parseUnits('1', 'ether'));
+      });
+
+      it("Should returns balance of 0 ether after the redeem function is invoked",  async () => {
+        const { owner } = await getSigners()
+
+        await (this.stablecoin.connect(owner).redeem(ethers.utils.parseUnits('1', 'ether')))
+        expect(await this.stablecoin.connect(owner).balanceOf(owner.address)).to.equal(ethers.utils.parseUnits('0', 'ether'));
+      });
+
+      it("Should returns totalSupply of 0 eth",  async () => {
+        const { owner } = await getSigners()
+
+        expect(await this.stablecoin.connect(owner).totalSupply()).to.equal(ethers.utils.parseUnits('0', 'ether'));
+      });
+    })
+  })
+
+  describe('getBlackListStatus', async() => {
+    it("Should returns account block is false",  async () => {
+      const { stablecoin, owner } = await loadFixture(deployStablecoin);
+
+      expect(await stablecoin.connect(owner).getBlackListStatus(owner.address)).to.equal(false);
+    });
+  })
+
+  describe('AddBlackList', function() {
+    it("Should returns error role access",  async () => {
+      const { stablecoin, owner, addr1, } = await loadFixture(deployStablecoin);
+
+      await expect(stablecoin.connect(addr1).addBlackList(owner.address)).to.be.revertedWith(/AccessControl: account .* is missing role .*/)
+    });
+
+    context('Trying add addr1 into blacklist', async () => {
+      before(async () => {
+        const { stablecoin } = await loadFixture(deployStablecoin);
+        this.stablecoin = stablecoin;
+      });
+
+      it("Should returns account addr1 block is true",  async () => {
+        const { owner, addr1 } = await getSigners()
   
+        await this.stablecoin.connect(owner).addBlackList(addr1.address);
+        expect(await this.stablecoin.connect(owner).getBlackListStatus(addr1.address)).to.equal(true);
+      });
+    })
+  }) 
+
+  describe('removeBlackList', function() {
+    it("Should returns error role access",  async () => {
+      const { stablecoin, owner, addr1, } = await loadFixture(deployStablecoin);
+
+      await expect(stablecoin.connect(addr1).removeBlackList(owner.address)).to.be.revertedWith(/AccessControl: account .* is missing role .*/)
+    });
+
+    context('Trying remove addr1 into blacklist', async () => {
+      before(async () => {
+        const { stablecoin, owner, addr1} = await loadFixture(deployStablecoin);
+
+        await stablecoin.connect(owner).addBlackList(addr1.address);
+
+        this.stablecoin = stablecoin;
+      });
+
+      it("Should returns account addr1 block is false",  async () => {
+        const { owner, addr1 } = await getSigners()
+        
+        expect(await this.stablecoin.connect(owner).getBlackListStatus(addr1.address)).to.equal(true);
+
+        await this.stablecoin.connect(owner).removeBlackList(addr1.address);
+
+        expect(await this.stablecoin.connect(owner).getBlackListStatus(addr1.address)).to.equal(false);
+      });
+    })
+  }) 
+
+  describe('destroyBlackFunds', function() {
+    it("Should returns error role access",  async () => {
+      const { stablecoin, owner, addr1, } = await loadFixture(deployStablecoin);
+
+      await expect(stablecoin.connect(addr1).destroyBlackFunds(owner.address)).to.be.revertedWith(/AccessControl: account .* is missing role .*/)
+    });
+
+    it("Should returns error user dont is in blacklist",  async () => {
+      const { stablecoin, owner, addr1, } = await loadFixture(deployStablecoin);
+
+      await expect(stablecoin.connect(owner).destroyBlackFunds(addr1.address)).to.be.revertedWith("Stablecoin: user dont is in blacklist")
+    });
+
+    context('Trying destroy funds of addr1', async () => {
+      before(async () => {
+        const { stablecoin, owner, addr1} = await loadFixture(deployStablecoin);
+
+        await stablecoin.connect(owner).mint(addr1.address, ethers.utils.parseUnits('1', 'ether'));
+        await stablecoin.connect(owner).addBlackList(addr1.address);
+
+        this.stablecoin = stablecoin;
+      });
+
+      it("Should returns balance wallet is zero",  async () => {
+        const { owner, addr1 } = await getSigners()
+
+        const oldTotalSupply = await this.stablecoin.connect(owner).totalSupply();
+        const balance = await this.stablecoin.connect(owner).balanceOf(addr1.address);
+        const newTotalSupply = oldTotalSupply - balance;
+
+        await this.stablecoin.connect(owner).destroyBlackFunds(addr1.address);
+
+        expect(await this.stablecoin.connect(owner).balanceOf(addr1.address)).to.equal(ethers.utils.parseUnits('0', 'ether'));
+        expect(await this.stablecoin.connect(owner).totalSupply()).to.equal(newTotalSupply);
+      });
+    })
+  }) 
 });
